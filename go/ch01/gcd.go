c1084fb28c59d6d4db7b569d74e4aa75855739cf
ypyf <t34@qq.com>
2018-05-03 08:07:07 +0800

    Update gcd.go

--- go/ch01/gcd.go
+++ go/ch01/gcd.go
@@ -3,11 +3,11 @@ package ch01
 // 扩展欧几里得算法
 // ax+bx=gcd(a,b)
 // 输入a,b，输出x,y,gcd(a,b)
-func ExtendedGcd(a, b int) (x, y, g int) {
+func ExtendedGcd(a, b int) (x, y, gcd int) {
 	if b == 0 {
 		return 1, 0, a
 	}
-	// b*x' + (a%b)*y' = g
-	x, y, g = ExtendedGcd(b, a%b)
-	return y, x - (a/b)*y, g
+	// b*x' + (a%b)*y' = ax + bx = gcd
+	x, y, gcd = ExtendedGcd(b, a%b)
+	return y, x - (a/b)*y, gcd
 }
